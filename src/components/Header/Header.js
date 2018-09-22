dc8603b0df6185df766612f2d90ce4b2233ca392
Jesse Remington <jlremington213@gmail.com>
2018-09-22 18:20:35 -0500

    placed Log Out button in center of header and conditionally rendered button to show only when user is signed in.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { USER_ACTIONS } from '../../redux/actions/userActions';
+import { triggerLogout } from '../../redux/actions/loginActions';
 
-const Header = () => (
-  <div className="instructions">
-    <div>
-      <h1 className="lead">SCORE CARD</h1>
-    </div>
-    <button
-      onClick={this.logout}
-    >
-      logout
-    </button>
-  </div>
-);
+const mapStateToProps = state => ({
+  user: state.user,
+});
 
-export default Header;
+
+
+class Header extends Component {
+
+  componentDidMount() {
+    this.props.dispatch({ type: USER_ACTIONS.FETCH_USER });
+  }
+
+  logout = () => {
+    this.props.dispatch(triggerLogout());
+  }
+
+  render() {
+    let logoutBTN = null;
+
+    if (this.props.user.userName) {
+      logoutBTN = (
+        <button
+          className="logoutBTN"
+          onClick={this.logout}
+        > Log Out
+      </button>);
+    }
+    return (
+      <div className="instructions">
+        <h1 className="lead">SCORE CARD</h1>
+          {logoutBTN}
+      </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps)(Header);
