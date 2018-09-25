8f4764c4928c8f0b45fe4adcfad104546ec3b388
Jesse Remington <jlremington213@gmail.com>
2018-09-25 13:55:32 -0500

    created buttons for playing another game with nav back home, or a sign out.

--- src/components/PlayerWinPage/PlayerWinPage.js
+++ src/components/PlayerWinPage/PlayerWinPage.js
@@ -6,50 +6,64 @@ import { triggerLogout } from '../../redux/actions/loginActions';
 
 
 const mapStateToProps = state => ({
-  user: state.user,
+    user: state.user,
 });
 
 class PlayerWinPage extends Component {
-  componentDidMount() {
-    this.props.dispatch({ type: USER_ACTIONS.FETCH_USER });
-  }
-
-  componentDidUpdate() {
-    if (!this.props.user.isLoading && this.props.user.userName === null) {
-      this.props.history.push('home');
+    componentDidMount() {
+        this.props.dispatch({ type: USER_ACTIONS.FETCH_USER });
     }
-  }
 
-  logout = () => {
-    this.props.dispatch(triggerLogout());
-  }
+    componentDidUpdate() {
+        if (!this.props.user.isLoading && this.props.user.userName === null) {
+            this.props.history.push('home');
+        }
+    }
 
-  render() {
-    let content = null;
+    logout = () => {
+        this.props.dispatch(triggerLogout());
+    }
 
-    if (this.props.user.userName) {
-      content = (
-        <div>
-          <h1
-            id="welcome"
-          >
-            Hey, { this.props.user.userName }!
+    handlePlayAgain = () => {
+        console.log('in handlePlayAgain');
+        /////////clear all arrays in stores///////////
+        this.props.history.push('/course');
+    } 
+
+    render() {
+        let content = null;
+
+        if (this.props.user.userName) {
+            content = (
+                <div>
+                    <h1
+                        id="welcome"
+                    >
+                        Hey, {this.props.user.userName}!
           </h1>
-          <h2>
-              YOU WIN!!!
+                    <h2>
+                        YOU WIN!!!
           </h2>
-          {/* <p>Your ID is: {this.props.user.id}</p> */}
-        </div>
-      );
-    }
+                    {/* <p>Your ID is: {this.props.user.id}</p> */}
+                </div>
+            );
+        }
 
-    return (
-      <div>
-        <Nav />
-        { content }
-      </div>
-    );
-  }
+        return (
+            <div>
+                <Nav />
+                {content}
+                <div>
+                    <button>
+                        PLAY AGAIN?
+                    </button>
+                    <button>
+                        DONE FOR TODAY
+                    </button>
+                </div>
+            </div>
+        );
+    }
 }
 
 // this allows us to use <App /> in index.js
